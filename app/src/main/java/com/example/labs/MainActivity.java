8f148b0f5551c18e44a04d9aa929f66455b542b8
newTomas <tom.tomas.36478119@gmail.com>
2021-12-09 13:10:00 +0700

    lab4

--- app/src/main/java/com/example/labs/MainActivity.java
+++ app/src/main/java/com/example/labs/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.labs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,13 +17,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
+        setContentView(R.layout.activity_main);
+        ImageView img = (ImageView)findViewById(R.id.animationView);
+        img.setBackgroundResource(R.drawable.rabbit_animation);
+        AnimationDrawable frameAnimation = (AnimationDrawable)
+                img.getBackground();
+        frameAnimation.setOneShot(false);
+
+        Button button = (Button) findViewById(R.id.button3);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(intent);
+                if(frameAnimation.isRunning()){
+                    frameAnimation.stop();
+                    button.setText("Start");
+                } else {
+                    frameAnimation.start();
+                    button.setText("Stop");
+                }
             }
         });
     }
