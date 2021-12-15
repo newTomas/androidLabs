aba260216c6dd4180ea47af0b635c214eb634db3
newTomas <tom.tomas.36478119@gmail.com>
2021-12-15 20:32:59 +0700

    lab7

--- app/src/main/java/com/example/labs/MainActivity.java
+++ app/src/main/java/com/example/labs/MainActivity.java
@@ -2,24 +2,27 @@ package com.example.labs;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.content.Intent;
-import android.graphics.drawable.AnimationDrawable;
-import android.media.MediaPlayer;
-import android.net.Uri;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.MediaController;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.VideoView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    String name = "";
-    final static String nameVariableKey = "NAME_VAR";
-    final static String textViewTexKey = "TEXT_VIEW";
+    int PERMISSION_REQUEST_CODE = 1;
+    String fileName = "content.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,30 +30,85 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState){
-        outState.putString(nameVariableKey, name);
-        TextView textView = findViewById(R.id.textOutput);
-        outState.putString(textViewTexKey, textView.getText().toString());
-        super.onSaveInstanceState(outState);
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(this, "Permissions should be granted", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState){
-        super.onRestoreInstanceState(savedInstanceState);
-        name = savedInstanceState.getString(nameVariableKey);
-        String textViewText = savedInstanceState.getString(textViewTexKey);
-        TextView dataView = findViewById(R.id.textOutput);
-        dataView.setText(textViewText);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode,
+                permissions,
+                grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Permission granted", Toast.LENGTH_SHORT) .show();
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Permission denied", Toast.LENGTH_SHORT) .show();
+            }
+        }
     }
 
-    public void saveField(View view){
-        TextView input = findViewById(R.id.textInput);
-        name = input.getText().toString();
+    public void buttonSaveClick(View view)
+    {
+        if(!checkPermission()){
+            requestPermission();
+            return;
+        }
+        try(FileOutputStream fos = openFileOutput(fileName,MODE_PRIVATE))
+        {
+            EditText textBox = (EditText) findViewById(R.id.save_text);
+            String text = textBox.getText().toString();
+            fos.write(text.getBytes());
+            fos.close();
+            Toast.makeText(this, "Текстовый файл успешно сохранён!",
+            Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+            Toast.makeText(this, "Файл не найден!",
+            Toast.LENGTH_SHORT).show();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+            Toast.makeText(this, "Ошибка сохранения файла!",
+            Toast.LENGTH_SHORT).show();
+        }
     }
 
-    public void restoreField(View view){
-        TextView output = findViewById(R.id.textOutput);
-        output.setText(name);
+    public void buttonOpenClick(View view)
+    {
+        if(!checkPermission()){
+            requestPermission();
+            return;
+        }
+        try(FileInputStream fin = openFileInput(fileName))
+        {
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String(bytes);
+            TextView textView = (TextView) findViewById(R.id.load_text);
+            textView.setText(text);
+            fin.close();
+        } catch (IOException ex)
+        {
+            Toast.makeText(this, ex.getMessage(),
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
